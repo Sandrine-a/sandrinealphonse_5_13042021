19f471f3b5bef3ebee1bf340b7a60208a6b7e783
Sandrine-a <sandrine_a971@yahoo.fr>
2021-07-28 22:42:21 +0200

    modification en 2 fonctions pour affichage articles

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,33 +1,17 @@
 console.log('hello you');
 
-//Variable 
-let list = document.getElementById('artcicles__list');
-
+let articlesList = [];
 //Recuperation liste articles api Meubles en chêne http://localhost:3000/api/furniture
- function getArticles() {
+function getArticles() {
   fetch('http://localhost:3000/api/cameras/')
     .then(function(res) {
       if (res.ok) {
-        console.log(res);
         return res.json();
       }
     })
-    .then(function(dataList) {
-      console.log(dataList[1]);
-      dataList.forEach(function(data) {
-        console.log(data)
-        //ajout des <div> pour article:
-        let article = document.createElement('article');
-        //ajout de la class card boostrap pour contenant article:
-        article.classList.add('card')
-        //ajout données affichées pour chaque article dans la card bootstrap:
-        article.innerHTML = `
-        <a href="#" class="card-body")> 
-        <h3 class="card-title">${data.name}</h3>
-        <img src="${data.imageUrl}" class="card-img"/>
-        <p>${data.price} €</p> </a>`
-        list.appendChild(article);
-      });
+    .then (function(articlesInTable) {
+      articlesList = (articlesInTable)
+      console.log(articlesList);
     })
     .catch(function(err) {
       console.log('une erreur détectée' + err);
@@ -35,6 +19,24 @@ let list = document.getElementById('artcicles__list');
       list.innerHTML = ` <p class="col text-info">Une erreur est survenue, nous vous prions de bien vouloir réessayer dans un instant.</P>` + err;
     })
 }
-getArticles()
+getArticles() 
 
-//Afficher article
+setTimeout(function articlesListDisplay() {
+  articlesList.forEach(function(items) {
+    console.log(items);
+    const list = document.getElementById('artcicles__list');
+    //ajout des <div> pour article:
+    const article = document.createElement('article');
+    //ajout de la class card boostrap pour contenant article:
+    article.classList.add('card','col-md-5','mx-2','my-2')
+    //ajout données affichées pour chaque article dans la card bootstrap:
+    article.innerHTML = `
+    <a href="#" class="card-body")> 
+    <h3 class="card-title">${items.name}</h3>
+    <img src="${items.imageUrl}" class="card-img"/>
+    <div>${items.price} €
+    <p class="underlineLink">Voir cet article</p> </div> </a> `
+    list.appendChild(article);
+  })
+}, 2000);
+articlesListDisplay()
