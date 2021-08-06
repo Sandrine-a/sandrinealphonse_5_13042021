03df0779167ae0b246065d48e9179404b0bfa6ab
Sandrine-a <sandrine_a971@yahoo.fr>
2021-08-07 00:55:35 +0200

    ajout champ nombre

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -16,7 +16,11 @@ let articleSelected = [];
 //ajout de l'alerte produit ajouté sous bouton
 const addButton = document.getElementById('ajout__btn');
 addButton.addEventListener('click', addOne);
-//
+//Nombre d'article:
+/*   //creation des nombres
+ const numbersSection = document.getElementById('nombre')
+numberSelect.addEventListener('change', changeNumberArticle)
+ */
 
 //appel get dans l'api avec l'url construit
 function getArticleSelected() {
@@ -29,6 +33,7 @@ function getArticleSelected() {
   .then (function(data) {
     //ajout des data dans variable articleSelected voir fichier data.js
     articleSelected = (data);//données globales
+    console.log(articleSelected);
     //options de personnalisation creation d'une variable pour stocker
     const opt = (data.lenses);
     options = opt;
@@ -40,19 +45,14 @@ function getArticleSelected() {
 }
 
 getArticleSelected()
-console.log(articleSelected);
 
 //ajout produit///////
 //variable pour l'icone panier
 const cartIcon = document.querySelector('.header__cart--icon');
-
 /////
 
 setTimeout(function articleSelectedDisplay() {
-  //selection de la section pour afficher article
-  console.log(section);
   //ajout dans le html des données
-  //ajout des options pour select
   section.innerHTML = `
   <img  class="card-img-top" src="${articleSelected.imageUrl}"/>
   <h3 class="card-title mt-3">${articleSelected.name}</h3>
@@ -64,41 +64,96 @@ setTimeout(function articleSelectedDisplay() {
       <select name="lentilles" id="lentilles">
       </select>
     </form>
+    <form>
+      <label for="nombre" class="col-12"> Nombre d'article </label>
+      <select name="nombre" id="nombre">
+      </select>
+    </form>
+    <div class="nombre__choisi">Nombre d'article: </div>
   </div> 
   `
-      for (let option in options) {
-      //creation des champs de personnalisation de l'article dans le slect créé précédemment
-      const customiseSection = document.getElementById('lentilles')
-      //ajout liste déroulante
-      const optionCustom = document.createElement('option');
-      //ajout inner HTML des champs custom radio boutons:
-      optionCustom.innerHTML =
-      `
-      <option value=${options[option]}>${options[option]}</option>
-      `
-      customiseSection.appendChild(optionCustom)
+    for (let option in options) {
+    //creation des champs de personnalisation de l'article dans le slect créé précédemment
+    const customiseSection = document.getElementById('lentilles')
+    //ajout liste déroulante
+    const optionCustom = document.createElement('option');
+    //ajout inner HTML des champs custom radio boutons:
+    optionCustom.innerHTML =
+    `
+    <option value=${options[option]}>${options[option]}</option>
+    `
+    customiseSection.appendChild(optionCustom)
+    }
+    //creation du select des quantités
+    const nombreMax = 11;
+    for(let num=1; num < nombreMax; num++) {
+    //ajout liste déroulante
+    const numberSelect = document.createElement('option');
+    //ajout inner HTML des champs custom radio boutons:
+    numberSelect.innerHTML =
+    `
+    <option>${num}</option>
+    `
+    numbersSection.appendChild(numberSelect)
     }
   return articleSelected
 }, 500)
 
-articleSelectedDisplay()
-
+///////fonction choix quantité
+/* //choix de la quantité
+async function changeNubmerArticle() {
+  const nombreChoisi = document.querySelector(".nombre__choisi")
+} */
 //au click sur bouton:
-function addOne() {
-  //modification de l'objet en json
-  let articleJSON = JSON.stringify(articleSelected)
+async function addOne() {
+  await getArticleSelected
+  //ajout du nombre dans l'élément panier du header
+  let headerCounter = document.getElementById("header__cart")
+  //creation de l'objet article avec quantité
+  let articleTest = {
+    name: articleSelected.name,
+    id: articleSelected._id,
+    imageUrl: articleSelected.imageUrl,
+    qty: 1
+  }
+  // variable pour modification en JSON
+  let articleJSON = JSON.stringify(articleTest)
+  //varaible pour setItem dans localStorage
+  let addArticle = localStorage.setItem("article", articleJSON);
+
+  //creation du tableau pour stocker article en localstorage:
+/*   if()
+  articleInLocalStorage =[];
+  articleInLocalStorage.push(articleTest);
+  addArticle;
+
+  console.log(articleInLocalStorage); */
+
+
+/*   articleInLocalStorage.push(articleTest);
+  console.log(articleInLocalStorage);
+  addArticle; */
+
+/*   console.log((articleTest));
+  //creation de l'objet article et  modification en JSON
+  let articleJSON = JSON.stringify(articleTest)
+  console.log(articleJSON);
   //ajout au localStorage:
-  localStorage.setItem("article", articleJSON);
-
-  //message de confirmation d'ajout dans section innerHTML ci-dessous:
-  const messageAjoutArticle = document.createElement("p")
-  messageAjoutArticle.classList.add("col-12", "text-center", "my-3", "text-info")
-  let sectionMessageAjout = document.getElementById('ajout')
-  
-  sectionMessageAjout.appendChild(messageAjoutArticle)
-
-  alert(`Article ajouté avec succès`)
-
+  let addArticle = localStorage.setItem("article", articleJSON);
+  //variale pour parser les articles enregistrés dans local storage
+  let articleInLocalStorage = JSON.parse(localStorage.getItem("article"));
+  if(articleInLocalStorage == null) {
+    //creation du tableau si local storage vide
+    //ajout de l'article
+    articleInLocalStorage.push(1, articleTest);
+    addArticle;
+    articleJSON;
+    console.log(articleInLocalStorage);
+  }else if (articleInLocalStorage != null && articleInLocalStorage.id == articleInLocalStorage.id) {
+    articleInLocalStorage.qty ++;
+    addArticle;
+    articleJSON;
+  } */
 }
 //ajout du bouton  panier
 /* function buttonGoToKart() {
@@ -106,7 +161,27 @@ function addOne() {
   for (let i = 0; i < localStorage.length ;i++)
 } */
 
-
+/* push article:
+//au click sur bouton:
+function addOne() {
+  //modification de l'objet en json
+  let articleJSON = JSON.stringify(articleSelected);
+  //ajout au localStorage:
+  const addArticle = localStorage.setItem("article", articleJSON);
+  //parse article dans localStorage
+  let articleInLocalStorage = JSON.parse(localStorage.getItem("article"))
+  
+  if(articleInLocalStorage) {
+    articleInLocalStorage.push(articleSelected);
+    articleJSON;
+    addArticle;
+  }else{ 
+    articleInLocalStorage = [];
+    articleInLocalStorage.push(articleSelected);
+    articleJSON;
+    addArticle;
+  }
+} */
 
 
 
