30986d0eca30bce35ea30820dce72f7b1fb66dc2
Sandrine-a <sandrine_a971@yahoo.fr>
2021-08-09 02:51:44 +0200

    modification localstorage avec qty

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -11,15 +11,18 @@ const urlArticle = "http://localhost:3000/api/cameras/" + removePoint;
 const section = document.getElementById('article__display')
 
 
-/* // BOUTON //
+// BOUTON //
 //ajout de l'alerte produit ajouté sous bouton
 const addButton = document.getElementById('ajout__btn');
-addButton.addEventListener('click', addOne); */
+addButton.addEventListener('click', addArticles);
 
 let articleSelected = {};
 
 //varaible panier 
 let cart = []
+
+//variable total
+let articleTotalSelected = {}
 
 //Nombre d'article:
   //creation du total des articles
@@ -74,19 +77,97 @@ articleSelectedDisplay()
 
 //fonction calcul du prix total à l'event change du nombre d'article
 function getAmountArticles() {
+  let articleTotal
   console.log(articleSelected.price);
-  const choiceNumbers = numbersSection.options[numbersSection.selectedIndex].value
+  //definition variable pour le nombre d'articles
+  let choiceNumbers = numbersSection.options[numbersSection.selectedIndex].value
+  //calcule du prix total:
   const price = articleSelected.price * choiceNumbers /100 ;
   console.log(price);
-  //ajout dans <span> du prix total:
+  //ajout dans <span> du prix total innerHTML:
   const resultTotal = document.querySelector('.total__prix')
   resultTotal.innerHTML = 
   `
   ${price} €
   `
+  //boucle pour creation d'un total
+  for(let num in choiceNumbers) {
+    articleTotal = {
+      name: articleSelected.name,
+      id: articleSelected._id,
+      price: articleSelected.price,
+      img: articleSelected.imageUrl,
+      qty: choiceNumbers[num],
+      totalprice: price
+    }
+  }
+  articleTotalSelected = articleTotal
+  console.log(articleTotalSelected );
 }
 
 //stockage des articles dans le localStorage
+async function addArticles() {
+  let choiceNumbers = numbersSection.options[numbersSection.selectedIndex].value
+  let articleStorage = localStorage.getItem('article')
+  //parse article dans localStorage
+  let articleInLocalStorage = JSON.parse(localStorage.getItem("article"))
+  // definition de la fonction pour ajouter un article dans le localStorage:
+  const addOne = () => {
+  articleInLocalStorage.push(articleTotalSelected)
+  //modification de l'objet en json et envoi dans key "article"
+  localStorage.setItem("article", JSON.stringify(articleInLocalStorage));
+}
+  //si selection nulle renvoie alert: border + message (to do)
+ if (choiceNumbers == "") {
+  numbersSection.style.border = "2px dashed red";
+ }else {
+   if(articleInLocalStorage) {
+    addOne()
+   }
+/*    else if(articleInLocalStorage){
+    console.log('coucouc');
+   } */
+   else {
+    //creation du tableau des article dans le pannier
+    articleInLocalStorage = []
+    addOne()
+
+    console.log(articleInLocalStorage);
+   }
+ }
+}
+
+
+/* /bon locaStorage sans prise en compte de la qty
+//stockage des articles dans le localStorage
+async function addArticles() {
+  let choiceNumbers = numbersSection.options[numbersSection.selectedIndex].value
+  let articleStorage = localStorage.getItem('article')
+  //parse article dans localStorage
+  let articleInLocalStorage = JSON.parse(localStorage.getItem("article"))
+
+  // definition de la fonction pour ajouter un article dans le localStorage:
+  const addOne = () => {
+  articleInLocalStorage.push(articleSelected)
+  //modification de l'objet en json et envoi dans key "article"
+  localStorage.setItem("article", JSON.stringify(articleInLocalStorage));
+}
+  //si selection nulle renvoie alert: border + message (to do)
+ if (choiceNumbers == "") {
+  numbersSection.style.border = "2px dashed red";
+ }else {
+   if(articleInLocalStorage) {
+    addOne()
+   }
+   else {
+    //creation du tableau des article dans le pannier
+    articleInLocalStorage = cart
+    addOne()
+
+    console.log(articleInLocalStorage);
+   }
+ }
+} */
 
 
 
