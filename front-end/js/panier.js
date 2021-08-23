29427903a25deeff08d5fe83c8313c6a0be1e789
Sandrine-a <sandrine_a971@yahoo.fr>
2021-08-23 13:41:39 -0400

    modification style panier et MAJ on cart avec nouveau localStorag

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -5,8 +5,10 @@ let articleInLocalStorage = JSON.parse(localStorage.getItem("article"));
 const btnClear = document.getElementById("order__btn");
 //section qui contient le panier:
 const displayCart = document.getElementById("panier__display");
+//dans section panier titre du tableau:
+const tabHeaderCart = document.getElementById("tab__header")
 //dans section panier affichage des articles et tarifs:
-const displayPrice = document.getElementById("panier__display--price")
+const displayPrice = document.getElementById("panier__display--content")
 //Dans section controle du panier, zone total:
 const displayTotalPrice =  document.querySelector(".control__panier--total")
 //Decalaration du tableau avec id du panier
@@ -62,28 +64,11 @@ function articlesInCartSome() {
     return id.push(idFilter.id);
   })
   filterById
-  let products = id
+  let products = id;
   //envoie objet products dans localstorage et transfomation en string:
-  localStorage.setItem("products", JSON.stringify(products))
-  
-/*   for (let i=0; i < articleInLocalStorage.length; i ++) {
-    while(articleInLocalStorage.length) {
-      if(articleInLocalStorage[i].id == articleInLocalStorage[i].id) {
-        console.log(articleInLocalStorage[i].price);
-        let some =0;
-        some += articleInLocalStorage[i].price;
-        console.log(some);
-        return some;
-        break;
-      }
-    }
-  } */
-
+  localStorage.setItem("products", JSON.stringify(products));
 
   ///3 - - creation de nouveau tableau par id nom et prix pour afficher en html:
-
-
-
 
 /*   for (let i=0; i < articleInLocalStorage.length; i ++) {
     console.log(articleInLocalStorage.length);
@@ -103,116 +88,57 @@ function articlesInCartSome() {
     createArticle.classList.add("row");
     createArticle.innerHTML = 
     `
-      <div class="col-4">
+      <div class="col-3">
         <p>${articleInLocalStorage[i].name}</p>
       </div>
-      <div class="col-4">
-        <p>  €</p>
+      <div class="col-3">
+        <p> ${articleInLocalStorage[i].price} €</p>
+      </div>
+      <div class="col-3">
+        <p> ${articleInLocalStorage[i].qty} </p>
+      </div>
+      <div class="col-3">
+        <input type="button" class="btn btn-light" id="btn__moins" value="-">
+        <input type="button" class="btn btn-light" id="btn__plus" value="+"> 
       </div>
     ` 
-    displayPrice.appendChild(createArticle) 
-  }
-  
-/*   for (let i=0; i < articleInLocalStorage.length; i ++) {
-    const filterById = articleInLocalStorage.filter(function(idFilter) {
-      console.log(idFilter.id);
-      return idFilter.id
-
-    })
-        //ajout des <articles> pour article:
-        const createArticle = document.createElement("article");
-        //ajout de la class row pour style boostrap pour contenant article:
-        createArticle.classList.add("row");
-        createArticle.innerHTML = 
-        `
-          <div class="col-4">
-            <p>${articleInLocalStorage[i].name}</p>
-          </div>
-          <div class="col-4">
-            <p>€</p>
-          </div>
-        ` 
-        displayPrice.appendChild(createArticle) 
-
-      }
+    displayPrice.appendChild(createArticle)
+    /// 5 - fonction supprimer article  
     
-/*     const mapById = articleInLocalStorage.map(function(idFilter) {
-      return idFilter.id
-    })
-    console.log(mapById );
-    mapById  */
-
-/*   for (let i=0; i < articleInLocalStorage.length; i ++) {
-    const filterById = articleInLocalStorage.filter(function(idFilter) {
-      return idFilter.id == articleInLocalStorage[i].id;
-    })
-    let idToCompare = articleInLocalStorage[i].id
-    console.log(filterById );
-    filterById
-  } */
-
-  //recherche de l'id de chaque article en locaStorage:
- /*  for (let i=0; i < articleInLocalStorage.length; i ++) {
-  idInitial = articleInLocalStorage[].id;
-  console.log(idInitial);
-  //calcule de la somme par id:
-  let some = articleInLocalStorage[i].price * idInitial;
-  console.log(some);
-    //// boucle pour faire la some par id:
-    switch(idInitial) {
-      case articleInLocalStorage[i].id: 
-      
-      let byArticle = []
-      byArticle.push(articleInLocalStorage)
-      console.log(byArticle);
-      //ajout des <articles> pour article:
-      const createArticle = document.createElement("article");
-      //ajout de la class row pour style boostrap pour contenant article:
-      createArticle.classList.add("row");
-      createArticle.innerHTML = 
-      `
-        <div class="col-4">
-          <p>${articleInLocalStorage[i].name}</p>
-        </div>
-        <div class="col-4">
-          <p> ${some} €</p>
-        </div>
-      ` 
-      displayPrice.appendChild(createArticle) 
-    }
-  } */
+  }
 }
 
 
 //bouton vider panier:
-async function clearCart() {
+async function clearCart(e) {
   await init()
   console.log('pour vider');
   //popup pour confirmer le panier vide:
   if (window.confirm("Souhaitez-vous supprimer tous les éléments de votre panier?")) {
     //on vide le localStorage
     localStorage.clear()
-    //puis actualisation de la page pour afficher le panier vide
-    location.reload()
   } else {
     console.log("pannier non confirmé vidage");
   };
+  //puis actualisation de la page pour afficher le panier vide
+  location.reload()
 }
 //init: pour affichage des produits dans onload: 
 const init = () => {
   //Affichage des produits dans popup panier:
   onCartEdit();
-  //affichage du contenu de la page: 
+  //affichage du contenu de la page en cas de panier vide: 
   if(articleInLocalStorage == null) {
   //affichage du message si panier vide:
-    const messageCartEmpty = document.getElementById('panier__display__message')
-    console.log('vide vide');
-    messageCartEmpty.classList.remove('hide')
+    const messageCartEmpty = document.getElementById('panier__display__message');
+    tabHeaderCart.classList.add('hide');
+    messageCartEmpty.classList.remove('hide');
   } else {
   //affichage des produits dans le html si panier rempli:
   articlesInCartSome();
   }
 }
+
 //fonction calcule du total de la commande:
 function totalOrder() {
   let tot = 0;
@@ -222,14 +148,6 @@ function totalOrder() {
     //affichage dans la section html:
   return displayTotalPrice.innerHTML = `${tot} €`;
 }
-
-
-/* 
-  map(function(name) {
-  return { value: name.name };
-})
-console.log(cartDisplay); */
-
 
 
 ///////PARTIE GESTION DU FORMULAIRE:
@@ -323,6 +241,7 @@ async function sentFormToServer(event) {
   })
 };
 
+
 /////fonctions activées
 //fonction onload
 window.onload = init;
@@ -330,48 +249,9 @@ window.onload = init;
 btnClear.addEventListener('click', clearCart);
 //prix total
 totalOrder()
+
 //formulaire:
 //fonction verification des champs:
 contactForm.addEventListener('submit', inputCheck)
-
-
-//////////////
-
-///filtrer selon le prix
-
-let newTest = [];
-///
-const cartDisplay = articleInLocalStorage.filter(function(price) { 
-const priceX = articleInLocalStorage[0].name; 
-if(price.name == priceX) {
-  return newTest.push(price)
-  } else {
-  return false
-  }
-})
-console.log(cartDisplay);
-console.log(newTest);
-///total par article
-/* const totalByArt = newTest.reduce((total, price) => {
-  return total + price.price;
-}, 0)
-console.log(totalByArt);
- */
-/* const nameT = articleInLocalStorage[0].name;
-const cartDisplay = articleInLocalStorage.filter(function (name) {  
-  return name.name == nameT
-})
-console.log(cartDisplay); */
-
-//total du tableau par article:
-
-
-
-//creation du nouveau tableau article avec qty
-/* const articleWithQty = articleInLocalStorage.map(function addQty(article) {
-  return article
-}) */
-
-///creation d'un total :
 
 
