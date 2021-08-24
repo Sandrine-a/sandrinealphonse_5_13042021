671b5d77ab3b5fea07a5a47e623000aff9a56483
Sandrine-a <sandrine_a971@yahoo.fr>
2021-08-24 02:30:51 -0400

    modification du panier: reglage qty.

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -1,6 +1,7 @@
 /////// partie 1 afficahge du panier:
 ///declaration des cariables réutilisables
 let articleInLocalStorage = JSON.parse(localStorage.getItem("article"));
+
 //bouton vider panier:
 const btnClear = document.getElementById("order__btn");
 //section qui contient le panier:
@@ -18,7 +19,6 @@ let id =[];
 function onCartEdit() {
   try {
     const articleInLocalStorage = JSON.parse(localStorage.getItem("article"))
-    console.log(articleInLocalStorage[0].qty)
     const cartElement = document.querySelector('.header__cart--popup')
 
     const thereIsArticlesInStorage = articleInLocalStorage && articleInLocalStorage.length > 0;
@@ -28,7 +28,6 @@ function onCartEdit() {
 
     if (thereIsArticlesInStorage && cartBadgeIsHidden) {
       /// On affiche la pop up avec le nombre d'articles
-      console.log(articleInLocalStorage);
       //-calcule des articles dans le panier:
       let totalArticles = 0;
       for (let qt in articleInLocalStorage) {
@@ -47,7 +46,7 @@ function onCartEdit() {
 
 }
 
-//calcule des sommes par elements dans panier et envoi objet products en localstorage:
+///calcule des sommes par elements dans panier et envoi objet products en localstorage:
 function articlesInCartSome() {
   ///1- - tri des articles par nom de produit
   const sortByName= articleInLocalStorage.sort((a, b) => {
@@ -65,52 +64,119 @@ function articlesInCartSome() {
   })
   filterById
   let products = id;
-  //envoie objet products dans localstorage et transfomation en string:
+  //Envoi objet products dans localstorage et transfomation en string:
   localStorage.setItem("products", JSON.stringify(products));
-
-  ///3 - - creation de nouveau tableau par id nom et prix pour afficher en html:
-
-/*   for (let i=0; i < articleInLocalStorage.length; i ++) {
-    console.log(articleInLocalStorage.length);
-    //
-    let some =0;
-    some += articleInLocalStorage[i].price;
-    console.log(some);
-    console.log(articleInLocalStorage[i].id); */
-
-    /// 4 - - envoie dans le html prix et produits
-    console.log(articleInLocalStorage);
-  
-    for (let i=0; i < articleInLocalStorage.length; i ++) {
-    //ajout des <articles> pour article:
-    const createArticle = document.createElement("article");
-    //ajout de la class row pour style boostrap pour contenant article:
-    createArticle.classList.add("row");
-    createArticle.innerHTML = 
-    `
-      <div class="col-3">
-        <p>${articleInLocalStorage[i].name}</p>
-      </div>
-      <div class="col-3">
-        <p> ${articleInLocalStorage[i].price} €</p>
-      </div>
-      <div class="col-3">
-        <p> ${articleInLocalStorage[i].qty} </p>
-      </div>
-      <div class="col-3">
-        <input type="button" class="btn btn-light" id="btn__moins" value="-">
-        <input type="button" class="btn btn-light" id="btn__plus" value="+"> 
-      </div>
-    ` 
-    displayPrice.appendChild(createArticle)
-    /// 5 - fonction supprimer article  
-    
+  /// 3 - - envoie dans le html prix et produits
+  for (let i=0; i < articleInLocalStorage.length; i ++) {
+      //ajout des <articles> pour article:
+      const createArticle = document.createElement("article");
+      //ajout de la class row pour style boostrap pour contenant article:
+      createArticle.classList.add("row", "cart__item");
+      createArticle.innerHTML = 
+      `
+        <div class="col-3 cart__item--name">
+          <p>${articleInLocalStorage[i].name}</p>
+        </div>
+        <div class="col-3">
+          <p> ${articleInLocalStorage[i].price} €</p>
+        </div>
+        <div class="col-3 cart__item--qty">
+          <p class="item__qty"> ${articleInLocalStorage[i].qty} </p>
+        </div>
+        <div class="col-3" id="cart__item--btn">
+          <input type="button" class="btn btn-light" id="btn__moins" value="-">
+          <input type="button" class="btn btn-light" id="btn__plus" value="+"> 
+        </div>
+      ` 
+      displayPrice.appendChild(createArticle)     
   }
 }
 
+///Changement des quantités par articles:
+function changeQuantity() {
+  //////// gestion quantités:
+  ///Recuperation des elements html boutons - et +:
+  const btnMoins = document.querySelectorAll("#btn__moins");
+  const btnPlus = document.querySelectorAll("#btn__plus");
+  
+  ///fonction pour le bouton moins:
+  for (let i = 0; i < btnMoins.length; i ++) {
+    //variable pour récupérer le nom de l'article liée au bouton:
+    const nameForBtnContainer = btnMoins[i].parentElement.parentElement.firstElementChild.firstElementChild.textContent;
+    console.log(nameForBtnContainer);
+    ///Evenement au click sur le bouton ciblé:
+    btnMoins[i].addEventListener('click', function decreaseQty(event) {
+      event.stopPropagation();
+      //On MAJ la quantité dans le local storage selon le nom qui correspond:
+      articleInLocalStorage.forEach(item => {
+        if(item.name === nameForBtnContainer) {
+          //On retire un produit:
+          item.qty --;
+          console.log(item.name);
+          console.log(item.qty);
+          //suppression de la ligne si valeur =0:
+          if (item.qty === 0) {
+            //demande de confirmation de suppression:
+            if(confirm(`Confirmez-vous la suppresion du ${item.name} ?`)) {
+              console.log(articleInLocalStorage);
+              console.log(articleInLocalStorage.length);
+              //Si articles totalement suppprimés, suppression du localStorage par filter:
+              articleInLocalStorage = articleInLocalStorage.filter((el)=> {
+                console.log(el.qty == 0);
+                return el.qty !== 0;
+              })
+            } 
+            //Si non confirmé, remise à 1 du compteur:
+            else {
+              item.qty ++;
+            }
+          }
+          //mise à jour du localStorage
+          localStorage.setItem("article", JSON.stringify(articleInLocalStorage));
+        };
+      }) 
+      totalOrder()
+      //on MAJ la page;
+      location.reload();
+    })
+  };
 
-//bouton vider panier:
+   ///fonction pour le bouton Plus
+  for (let i = 0; i < btnPlus.length; i ++) {
+    //variable pour récupérer le nom de l'article liée au bouton:
+    const nameForBtnContainer = btnMoins[i].parentElement.parentElement.firstElementChild.firstElementChild.textContent;
+    console.log(nameForBtnContainer);
+    btnPlus[i].addEventListener('click', function increaseQty(event) {
+      event.stopPropagation();
+      //On récupère la quantité dans le HTML et met à jour:
+      articleInLocalStorage.forEach(item => {
+        if(item.name === nameForBtnContainer) {
+          //On retire ajoute produit:
+          item.qty ++;
+          console.log(item.name);
+          console.log(item.qty);
+          //Nombre maximum: 20cam
+          if (item.qty > 20) {
+            //demande de confirmation de suppression:
+            window.alert(`Vous avez atteint le maximum de ${item.name}`)
+            //Bloquage du bouton +:
+            btnPlus[i].setAttribute("disabled","disabled")
+          } else {
+            btnPlus[i].removeAttribute("disabled")
+          }
+          //ON MAJ la qty correspondant dans le localStorage
+          localStorage.setItem("article", JSON.stringify(articleInLocalStorage))
+          totalOrder() 
+        }
+      })
+      //on MAJ la page;
+      location.reload();
+    })
+  };
+};
+///bouton vider panier:
 async function clearCart(e) {
+  e.stopPropagation()
   await init()
   console.log('pour vider');
   //popup pour confirmer le panier vide:
@@ -123,27 +189,34 @@ async function clearCart(e) {
   //puis actualisation de la page pour afficher le panier vide
   location.reload()
 }
+
 //init: pour affichage des produits dans onload: 
 const init = () => {
   //Affichage des produits dans popup panier:
   onCartEdit();
   //affichage du contenu de la page en cas de panier vide: 
-  if(articleInLocalStorage == null) {
-  //affichage du message si panier vide:
+  if(articleInLocalStorage == null || articleInLocalStorage.length == 0 ) {
+    //si le panier est vidé individuellement par les btn -:
+    localStorage.clear()
+    //affichage du message si panier vide:
     const messageCartEmpty = document.getElementById('panier__display__message');
     tabHeaderCart.classList.add('hide');
     messageCartEmpty.classList.remove('hide');
   } else {
   //affichage des produits dans le html si panier rempli:
   articlesInCartSome();
-  }
+  //prix total
+  totalOrder();
+  //Gere la quantité:
+  changeQuantity()
+}
 }
 
 //fonction calcule du total de la commande:
-function totalOrder() {
+function totalOrder(article) {
   let tot = 0;
   for (let p in articleInLocalStorage) {
-    tot += articleInLocalStorage[p].price;
+    tot = articleInLocalStorage[p].price * articleInLocalStorage[p].qty;
   }
     //affichage dans la section html:
   return displayTotalPrice.innerHTML = `${tot} €`;
@@ -247,11 +320,11 @@ async function sentFormToServer(event) {
 window.onload = init;
 //bouton vider panier:
 btnClear.addEventListener('click', clearCart);
-//prix total
-totalOrder()
 
 //formulaire:
 //fonction verification des champs:
 contactForm.addEventListener('submit', inputCheck)
+
+console.log(articleInLocalStorage);
 
 
